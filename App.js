b19147b7ef3b5ee33758cbe79fd5f46b7ed06ca9
junjun <junjun@baicizhan.com>
2018-06-14 15:10:05 +0800

    ok

---
+++ App.js
@@ -0,0 +1,113 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  Platform,
+  StyleSheet,
+  Text,
+  View,
+  Button
+} from 'react-native';
+
+import {NativeModules} from "react-native"
+
+//在objective-c中使用"RCT_EXPORT_MODULE(MyNativeLibModule)"我们使用 MyNativeLibModule
+const MyNativeLibModule = NativeModules.MyNativeLibModule
+
+
+
+const instructions = Platform.select({
+  ios: 'Press Cmd+R to reload,\n' +
+    'Cmd+D or shake for dev menu',
+  android: 'Double tap R on your keyboard to reload,\n' +
+    'Shake or press menu button for dev menu',
+});
+
+export default class App extends Component {
+
+  constructor()
+  {
+    super();
+  }
+
+  /*
+    获取IMSI码
+    在native代码中使用RCT_EXPORT_METHOD(getIMSI:  resolver:(RCTPromiseResolveBlock)resolve  rejecter:(RCTPromiseRejectBlock)reject)，返回的是一个promise
+    我们在js代码这样使用中
+   */
+
+  getIMSI()
+  {
+       //使用promise
+       MyNativeLibModule.getIMSIPromise().then(val => {
+         alert(`成功获取IMSI:${val}`)
+       }).catch(e => {
+         console.error(e);
+       })
+  }
+
+  getAppversion()
+  {
+    //使用一个匿名函数作为callback
+    MyNativeLibModule.getAppVersionCallback( (val,e)=> {
+      if(e)
+      {
+        console.error(e);
+      }
+      else
+      {
+        alert(`成功获取AppVersion:${val}`)
+      }
+    });
+  }
+
+
+  /**
+   * 耗时的方法
+   */
+  getResponsePromise()
+  {
+    //使用promise
+    MyNativeLibModule.getResponsePromise().then(val => {
+      alert(`成功获取response(非常耗时):${val}`)
+    }).catch(e => {
+      console.error(e);
+    })
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+
+
+        <Button title={"获取IMSI"} onPress={()=>{this.getIMSI()}}></Button>
+        <Button title={"获取Appversion"} onPress={()=>{this.getAppversion()}}></Button>
+        <Button title={"获取response(耗时)"} onPress={()=>{this.getResponsePromise()}}></Button>
+
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
---
+++ ios/rn_demo_1/myNativeLib.m
@@ -0,0 +1,119 @@
+//
+//  myNativeLib.m
+//  rn_demo_1
+//
+//  Created by junjun on 2018/6/14.
+//  Copyright © 2018年 Facebook. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "React/RCTLog.h"
+#import <React/RCTConvert.h>
+
+#if __has_include("RCTBridgeModule.h")
+  #import "RCTBridgeModule.h"
+#else
+  #import <React/RCTBridgeModule.h>
+#endif
+
+#import <CoreTelephony/CTCarrier.h>
+#import <CoreTelephony/CTTelephonyNetworkInfo.h>
+
+
+//定义类
+@interface MyNativeLib : NSObject <RCTBridgeModule>
+
+@end
+
+
+//类实现
+@implementation MyNativeLib
+
+//在javascript端使用时候module名字叫MyNativeLibModule,如果参数为空，会默认使用类名
+RCT_EXPORT_MODULE(MyNativeLibModule)
+
+//相当于一个 js module export出去的方法 ,在js中使用RCTPromiseResolveBlock().then().catch()调用
+RCT_EXPORT_METHOD(getIMSIPromise: (RCTPromiseResolveBlock)resolve  rejecter:(RCTPromiseRejectBlock)reject    )
+{
+  NSString * imsi = nil;
+  @try {
+    
+    /**
+     ......
+     获取imsi逻辑
+     ......
+     */
+    
+    imsi = @"46000";
+    resolve(imsi);
+    
+  } @catch (NSException *exception) {
+     reject(@"", @"失败", nil);
+  }
+}
+
+
+//相当于一个 js module export出去的方法 在js中使用 getAppVersionCallback(function (val,e){})调用
+RCT_EXPORT_METHOD(getAppVersionCallback:(RCTResponseSenderBlock)callback)
+{
+  NSString * version = nil;
+  @try {
+    /**
+     ......
+     获取vappersion逻辑
+     ......
+     */
+    
+   version = @"1.0.5";
+   //callback只接受一个数组做参数
+   callback(@[version,[NSNull null]]);
+    
+  } @catch (NSException *exception) {
+    callback(@[version,exception]);
+  }
+}
+
+
+//非常耗时的方法需要使用async的方式让一个线程去执行
+
+//指定方法执行的队列
+//
+- (dispatch_queue_t)methodQueue
+{
+  return dispatch_get_main_queue();//指定为主线程(UI线程)
+}
+
+//很耗时的方法,使用异步 ,在js中使用RCTPromiseResolveBlock().then().catch()调用
+RCT_EXPORT_METHOD(getResponsePromise: (RCTPromiseResolveBlock)resolve  rejecter:(RCTPromiseRejectBlock)reject    )
+{
+    /**
+     ......
+     获取imsi逻辑
+     ......
+     */
+  
+    //使用global_queue
+    dispatch_queue_t myQueue = dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0);
+  
+    //业务逻辑放入queue仲执行
+    dispatch_async(myQueue, ^{
+       @try {
+          NSString * response = nil;
+          /*假装获取网络资源需要3秒钟*/
+          sleep(3);
+        
+          response = @"response is {data:123}";
+          resolve(response);
+         
+       } @catch (NSException *exception) {
+         reject(@"", @"注册失败", nil);
+       }
+    });
+
+    
+ 
+}
+
+@end
+
+
